1257b2098ad08db241960ad43c379dcb77448e6f
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-26 12:11:05 +0400

    12

--- Warmup/notString.java
+++ Warmup/notString.java
@@ -1,9 +1,14 @@
-public class 21 {
-    
-}
-
-/*
-    public String notString(String str) {
+public class notString {
+    public static void main(String[] args) {
+        notString ns = new notString(); 
+        System.out.println(ns.notString("candy")); // "not candy"
+        System.out.println(ns.notString("x")); // "not x"
+        System.out.println(ns.notString("not bad")); // "not bad"
+        System.out.println(ns.notString("not")); // "not"
+        System.out.println(ns.notString("not not bad")); // "not not bad"
+        System.out.println(ns.notString("not bad not")); // "not bad not"}
+    }
+        public String notString(String str) {
         String [] ss = str.split(" ");
         if (ss.length >0) {
             if (ss[0].equals("not")){
@@ -12,6 +17,10 @@ public class 21 {
         }
         return "not " + str;
     }
+}
+
+/*
+
     public String missingChar(String str, int n) {
         String str1= str.substring(0,n);
         String str2= str.substring(n+1,str.length());
