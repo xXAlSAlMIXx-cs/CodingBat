b372f8d247ca7c13e0bea6345d6e69080b0e9d83
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-28 12:11:47 +0400

    teaParty

---
+++ Logic-1/teaParty.java
@@ -0,0 +1,11 @@
+public class teaParty {
+    public int teaParty(int tea, int candy) {
+        if (tea < 5 || candy < 5) {
+            return 0;
+        }
+        if (tea >= 2 * candy || candy >= 2 * tea) {
+            return 2;
+        }
+        return 1;
+    }
+}
