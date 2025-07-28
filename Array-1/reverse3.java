70e4e2d8693446165758ab2bdbf10c23fc7e873d
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-28 10:30:28 +0400

    reverse3

---
+++ Array-1/reverse3.java
@@ -0,0 +1,9 @@
+public class reverse3 {
+    public int[] reverse3(int[] nums) {
+        int[] result = new int[3];
+        result[0] = nums[2];
+        result[1] = nums[1];
+        result[2] = nums[0];
+        return result;
+    }    
+}
