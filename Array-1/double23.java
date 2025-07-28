bd89d2f92d2cce2b6db1bd08847a9bed63d9bee9
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-28 10:54:28 +0400

    double23

---
+++ Array-1/double23.java
@@ -0,0 +1,5 @@
+public class double23 {
+    public boolean double23(int[] nums) {
+        return (nums.length == 2 && ((nums[0] == 2 && nums[1] == 2) || (nums[0] == 3 && nums[1] == 3)));
+    }
+}
