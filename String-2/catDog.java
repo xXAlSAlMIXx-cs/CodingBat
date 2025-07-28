9bcbe5cfed3ed04da76485384af6431477b1b80c
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-28 13:46:36 +0400

    catDog

---
+++ String-2/catDog.java
@@ -0,0 +1,15 @@
+public class catDog {
+    public boolean catDog(String str) {
+        int catCount = 0;
+        int dogCount = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i, i + 3).equals("cat")) {
+                catCount++;
+            }
+            if (str.substring(i, i + 3).equals("dog")) {
+                dogCount++;
+            }
+        }
+        return catCount == dogCount;
+    }
+}
