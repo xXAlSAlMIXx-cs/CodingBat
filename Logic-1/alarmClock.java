4c3705df8865cde85306ef0a510334675b83a8c2
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-28 11:57:53 +0400

    alarmClock and sortaSum

---
+++ Logic-1/alarmClock.java
@@ -0,0 +1,17 @@
+public class alarmClock {
+    public String alarmClock(int day, boolean vacation) {
+        if (vacation) {
+            if (day == 0 || day == 6) {
+                return "off";
+            } else {
+                return "10:00";
+            }
+        } else {
+            if (day == 0 || day == 6) {
+                return "10:00";
+            } else {
+                return "7:00";
+            }
+        }
+    }    
+}
