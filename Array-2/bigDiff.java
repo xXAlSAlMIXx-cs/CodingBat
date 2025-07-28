917f01f4c587ebdd47ec86894cca4a6a8b38d447
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-28 20:11:07 +0400

    bigDiff

---
+++ Array-2/bigDiff.java
@@ -0,0 +1,16 @@
+public class bigDiff {
+    public int bigDiff(int[] nums) {
+        int minmum = Integer.MAX_VALUE;
+        int maximum = Integer.MIN_VALUE;
+        for (int n : nums) {
+            if (n < minmum) {
+                minmum = n;
+        }
+        if (n > maximum) {
+            maximum = n;
+        }
+    }
+    return maximum - minmum;
+    }
+
+}
