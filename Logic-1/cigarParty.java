172fdefb50237f5fa4abd7e2b90fd845ca314486
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-28 11:54:23 +0400

    cigarParty

---
+++ Logic-1/cigarParty.java
@@ -0,0 +1,9 @@
+public class cigarParty {
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend) {
+            return cigars >= 40;
+        } else {
+            return cigars >= 40 && cigars <= 60;
+        }
+    }
+}
