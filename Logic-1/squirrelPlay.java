f7a585a78985061cfff80b990da76dd3ea7476b8
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-28 11:56:02 +0400

    squirrelPlay

---
+++ Logic-1/squirrelPlay.java
@@ -0,0 +1,9 @@
+public class squirrelPlay {
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+        if (isSummer) {
+            return temp >= 60 && temp <= 100;
+        } else {
+            return temp >= 60 && temp <= 90;
+        }
+    }
+}
