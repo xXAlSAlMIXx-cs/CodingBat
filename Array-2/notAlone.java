3d5103cbac2cb50c4ba6882d17e9c00ccc2ddf63
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-29 12:07:41 +0400

    notAlone

---
+++ Array-2/notAlone.java
@@ -0,0 +1,10 @@
+public class notAlone {
+    public int[] notAlone(int[] nums, int val) {
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] == val && nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+                nums[i] = Math.max(nums[i - 1], nums[i + 1]);
+            }
+        }
+        return nums;
+    }
+}
