c93dad80fac66897b4b4fa56acdb7cb084414271
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-29 11:35:15 +0400

    sameEndstripleUp

---
+++ Array-2/tripleUp.java
@@ -0,0 +1,13 @@
+public class tripleUp {
+    public boolean tripleUp(int[] nums) {
+        if(nums.length<3){
+            return false;
+        }
+        for (int i = 0; i < nums.length-2; i++) {
+            if(nums[i+1]==(nums[i]+ 1) &&(nums[i+2]==nums[i]+2)){
+                return true;
+            }
+        }
+        return false;
+    }   
+}
