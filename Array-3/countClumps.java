43125b9e49d45bd1fb351fc0858099c1e12613c6
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-30 21:58:31 +0400

    countClumps

--- Array-3/countClumps.java
+++ Array-3/countClumps.java
@@ -1,5 +1,21 @@
 public class countClumps {
     public int countClumps(int[] nums) {
-        return 0;
+        if (nums.length < 2) {
+            return 0;
+        }
+        int clumpCount = 0;
+        boolean inClump = false;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                if (!inClump) {
+                    clumpCount++;
+                    inClump = true;
+                }
+            } else {
+                inClump = false;
+            }
+        }
+        return clumpCount;
     }
+
 }
