a6c41d3dc26dce5c12bc5ae6f872bd00fed2c973
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-30 22:28:49 +0400

    count8

---
+++ Recursion_1/count8.java
@@ -0,0 +1,17 @@
+package Recursion_1;
+
+public class count8 {
+       public int count8(int n) {
+        if(n<=0){
+            return 0;
+        }
+        if(n%10==8 && (n/10)%10==8) {
+            return 2+count8(n/10);
+        }
+        if (n%10==8) {
+            return 1+ count8(n/10);
+        }
+        return count8(n/10);
+    }
+ 
+}
