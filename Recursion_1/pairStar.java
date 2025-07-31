0d47be8f49bcce850028f272f2b07b4a772ecbb8
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-31 11:26:21 +0400

    pairStar

--- Recursion_1/pairStar.java
+++ Recursion_1/pairStar.java
@@ -2,6 +2,12 @@ package Recursion_1;
 
 public class pairStar {
     public String pairStar(String str) {
-        
+        if (str.length()<=1) {
+            return str;
+        }   
+        if (str.charAt(0)==str.charAt(1)) {
+            return str.charAt(0)+"*"+pairStar(str.substring(1));
+        }
+        return str.charAt(0)+pairStar(str.substring(1));
     }
 }
