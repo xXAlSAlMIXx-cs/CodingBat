ae838ecf25673fc6a68c30d9024d9605bec569c3
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-07-31 13:31:26 +0400

    strDist

--- Recursion_1/strDist.java
+++ Recursion_1/strDist.java
@@ -2,6 +2,19 @@ package Recursion_1;
 
 public class strDist {
     public int strDist(String str, String sub) {
-        
+        if (str.length()<sub.length()) {
+            return 0;
+        }
+        if (str.equals(sub)) {
+            return str.length();
+        }
+
+        if (str.startsWith(sub)&& str.endsWith(sub)) {
+            return str.length();
+        }
+        if (!str.startsWith(sub)) {
+            return strDist(str.substring(1), sub);
+        }
+        return strDist(str.substring(0,str.length()-1), sub);
     }
 }
