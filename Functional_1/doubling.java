6838b078875800a1b4adbcea3c1eca721ab2e03f
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-08-01 16:53:41 +0400

    doubling

---
+++ Functional_1/doubling.java
@@ -0,0 +1,10 @@
+package Functional_1;
+
+import java.util.List;
+
+public class doubling {
+    public List<Integer> doubling(List<Integer> nums) {
+        nums.replaceAll(n -> n*2);
+        return nums;
+    }
+}
