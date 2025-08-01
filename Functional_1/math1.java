2b57ffdd035e7b59959d310dacf6fa81380a15c3
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-08-01 17:00:28 +0400

    math1

---
+++ Functional_1/math1.java
@@ -0,0 +1,10 @@
+package Functional_1;
+
+import java.util.List;
+
+public class math1 {
+    public List<Integer> math1(List<Integer> nums) {
+        nums.replaceAll(n -> (n+1)*10);
+        return nums;
+    }
+}
