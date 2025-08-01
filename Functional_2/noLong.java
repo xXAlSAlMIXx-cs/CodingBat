0c66c8db10b6a57def4e311664b6750865b64b77
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-08-01 17:13:24 +0400

    noLong

--- Functional_2/noLong.java
+++ Functional_2/noLong.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class noLong {
     public List<String> noLong(List<String> strings) {
-        strings.removeIf(n -> n.length()==4);
+        strings.removeIf(n -> n.length()>=4);
         return strings;
 }
 
