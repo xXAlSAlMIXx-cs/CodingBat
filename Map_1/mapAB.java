ff9085200f079666ab209f2e57dac98dda1a8776
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-08-01 12:10:51 +0400

    mapAB

---
+++ Map_1/mapAB.java
@@ -0,0 +1,13 @@
+package Map_1;
+
+import java.util.Map;
+
+public class mapAB {
+    public Map<String, String> mapAB(Map<String, String> map) {
+        if (map.containsKey("a")&&map.containsKey("b")) {
+            map.put("ab", map.get("a")+map.get("b"));
+        }
+        return map;
+    }
+
+}
