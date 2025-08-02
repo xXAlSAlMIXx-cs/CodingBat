c3d663e877f9820bbfa7d24fe9cc071ede14633c
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-08-02 15:59:07 +0400

    splitOdd10

--- Recursion_2/splitOdd10.java
+++ Recursion_2/splitOdd10.java
@@ -12,18 +12,15 @@ public class splitOdd10 {
     }
     
     public boolean summing(int[] nums, int index, int group1Sum, int group2Sum) {
-        // Base case: if all elements are processed, check conditions
         if (index >= nums.length) {
             return (group1Sum % 10 == 0 && group2Sum % 2 == 1) || 
                    (group2Sum % 10 == 0 && group1Sum % 2 == 1);
         }
         
-        // Try adding current number to Group 1
         if (summing(nums, index + 1, group1Sum + nums[index], group2Sum)) {
             return true;
         }
         
-        // Try adding current number to Group 2
         if (summing(nums, index + 1, group1Sum, group2Sum + nums[index])) {
             return true;
         }
