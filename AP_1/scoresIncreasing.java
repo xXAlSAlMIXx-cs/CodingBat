c1d725152e501781386268cacf10d07c3fc66a54
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-08-02 16:12:20 +0400

    scoresIncreasing

---
+++ AP_1/scoresIncreasing.java
@@ -0,0 +1,12 @@
+package AP_1;
+
+public class scoresIncreasing {
+    public boolean scoresIncreasing(int[] scores) {
+        for (int i = 0; i < scores.length-1; i++) {
+            if (scores[i]> scores[i+1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
