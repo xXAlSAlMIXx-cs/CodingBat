71748a46a1d13dd428c1ffb159ff113b6729f35e
xXAlSAlMIXx-cs <xxalsalmixx9099@gmail.com>
2025-08-02 16:45:57 +0400

    update scores INcreasing

--- AP_1/scoresIncreasing.java
+++ AP_1/scoresIncreasing.java
@@ -3,7 +3,7 @@ package AP_1;
 public class scoresIncreasing {
     public boolean scoresIncreasing(int[] scores) {
         for (int i = 0; i < scores.length-1; i++) {
-            if (scores[i]==100 && scores[i+1]=100) {
+            if (scores[i]==100 && scores[i+1]==100) {
                 return true;
             }
         }
